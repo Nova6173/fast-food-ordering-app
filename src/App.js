084a88aa8565d8b1dc663970f52775ac356855a8
Jeanette S <nova777nova@hotmail.com>
2024-08-08 11:57:46 +0200

    added pictures and some css to adjust pictures to fit the page

--- src/App.js
+++ src/App.js
@@ -7,14 +7,14 @@ import './App.css';
 
 const App = () => {
   const [items] = useState([
-    { id: 1, name: 'Burger', description: 'A delicious burger with all the fixings.', price: 7.99, image: 'burger-image-url' },
-    { id: 2, name: 'Pizza', description: 'Classic pizza with your favorite toppings.', price: 9.99, image: 'pizza-image-url' },
-    { id: 3, name: 'Fries', description: 'Crispy golden fries served hot and fresh.', price: 5.99, image: 'fries-image-url' },
-    { id: 4, name: 'Wings', description: 'Crispy wings served with your choice of protein.', price: 6.99, image: 'wings-image-url' },
-    { id: 5, name: 'Salad', description: 'Fresh salad with your choice of protein.', price: 4.99, image: 'salad-image-url' },
-    { id: 6, name: 'Milkshake Vanilla', description: 'Vanilla milkshake with your choice of sprinkles.', price: 3.99, image: 'milkshake-image-url' },
-    { id: 7, name: 'Milkshake Chocolate', description: 'Chocolate milkshake with your choice of sprinkles.', price: 3.99, image: 'milkshake-image-url' },
-    { id: 8, name: 'All you can eat menu', description: 'All you can eat menu', price: 20.00, image: 'all-you-can-eat-image-url' },
+    { id: 1, name: 'Burger', description: 'A delicious burger with all the fixings.', price: 7.99, image: '/images/burger-8933592_1280.jpg' },
+    { id: 2, name: 'Pizza', description: 'Classic pizza with your favorite toppings.', price: 9.99, image: '/images/pizza-8944546_1280.jpg' },
+    { id: 3, name: 'Fries', description: 'Crispy golden fries served hot and fresh.', price: 5.99, image: '/images/french-fries-4977353_1280.jpg' },
+    { id: 4, name: 'Wings', description: 'Crispy wings served with your choice of protein.', price: 6.99, image: '/images/chicken-wings-4000442_1280.jpg' },
+    { id: 5, name: 'Salad', description: 'Fresh salad with your choice of protein.', price: 4.99, image: '/images/pasta-7209002_1280.jpg' },
+    { id: 6, name: 'Milkshake Vanilla', description: 'Vanilla milkshake with your choice of sprinkles.', price: 3.99, image: '/images/milkshake-6578503_1280.jpg' },
+    { id: 7, name: 'Milkshake Chocolate', description: 'Chocolate milkshake with your choice of sprinkles.', price: 3.99, image: '/images/chocolate-smoothie-1058191_1280.jpg' },
+    { id: 8, name: 'All you can eat menu', description: 'All you can eat menu', price: 20.00, image: '/images/emoji-8935542_1280.png' },
   ]);
 
   const [order, setOrder] = useState([]);
