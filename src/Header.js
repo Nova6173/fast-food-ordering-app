be6b62010bfe10f72fcab6d83feaf11ca973928b
Jeanette S <nova777nova@hotmail.com>
2024-08-08 10:31:09 +0200

    added a header

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+// src/Header.js
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className="bg-primary text-white text-center py-4 mb-4">
+      <h1 className="display-4">Fast Food Menu</h1>
+    </header>
+  );
+};
+
+export default Header;
